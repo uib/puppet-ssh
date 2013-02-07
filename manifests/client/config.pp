216dc1d8cf51c0da877ce161d5e9c35d5ee10b26
Anders Vaage <anders.vaage@adm.uib.no>
2013-02-07 10:31:22 +0100

    Separate config and templates for OS X

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -1,10 +1,22 @@
 class ssh::client::config {
-  file { $ssh::params::ssh_config:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    source  => "puppet:///modules/${module_name}/ssh_config",
-    require => Class['ssh::client::install'],
+  
+  if ($::osfamily != Darwin) {
+    file { $ssh::params::ssh_config:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      source  => "puppet:///modules/${module_name}/ssh_config",
+      require => Class['ssh::client::install'],
+    }
+  }
+
+  if ($::osfamily == Darwin) {
+    file { $ssh::params::ssh_config:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'wheel',
+      source  => "puppet:///modules/${module_name}/ssh_config_darwin",
+    }
   }
 
   # Workaround for http://projects.reductivelabs.com/issues/2014
--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,12 +1,25 @@
 class ssh::server::config {
-  file { $ssh::params::sshd_config:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    replace => true,
-    source  => "puppet:///modules/${module_name}/sshd_config",
-    require => Class['ssh::server::install'],
-    notify  => Class['ssh::server::service'],
+  if ($::osfamily != Darwin) {
+    file { $ssh::params::sshd_config:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      replace => true,
+      source  => "puppet:///modules/${module_name}/sshd_config",
+      require => Class['ssh::server::install'],
+      notify  => Class['ssh::server::service'],
+    }
+  }
+
+  if ($::osfamily != Darwin) {
+    file { $ssh::params::sshd_config:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0644',
+      replace => true,
+      source  => "puppet:///modules/${module_name}/sshd_config_darwin",
+    }
   }
 }
